07a8e6b5019730b1ee1ee6688302ee9524f580e2
manhtran2103 <manhtran2103@gmail.com>
2018-01-17 12:58:41 +0200

    task1-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MainContentComponent,
+    SidebarComponent,
+    FooterComponent
+  
   ],
   imports: [
     BrowserModule
